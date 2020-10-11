911dd9231147b93179d9bc870c5778e48f5daeb8
anh288 <anh2073@naver.com>
2020-10-11 15:19:55 +0900

    JS : Work

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
     if (window.scrollY > navbarHeight) {
-    navbar.classList.add('navbar--dark');
+        navbar.classList.add('navbar--dark');
     } else {
-    navbar.classList.remove('navbar--dark');
+        navbar.classList.remove('navbar--dark');
     }
 });
 
@@ -17,7 +17,7 @@ navbarMenu.addEventListener('click', (event) => {
     const target = event.target;
     const link = target.dataset.link;
     if (link == null) {
-    return;
+        return;
     }
     navbarMenu.classList.remove('open');
     scrollIntoView(link);
@@ -40,15 +40,45 @@ document.addEventListener('scroll', () => {
 const arrowUp = document.querySelector('.arrow-up');
 document.addEventListener('scroll', () => {
     if (window.scrollY > homeHeight / 2) {
-    arrowUp.classList.add('visible');
+        arrowUp.classList.add('visible');
     } else {
-    arrowUp.classList.remove('visible');
+        arrowUp.classList.remove('visible');
     }
 });
 
 // Handle click on the "arrow up" button
 arrowUp.addEventListener('click', () => {
     scrollIntoView('#home');
+});
+
+// Projects
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) => {
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if (filter == null) {
+        return;
+    }
+
+    // Remove selection from the previous item and select the new one
+    const active = document.querySelector('.category__btn.selected');
+    if (active != null) {
+        active.classList.remove('selected');
+    }
+    e.target.classList.add('selected');
+
+    projectContainer.classList.add('anim-out');
+    setTimeout(() => {
+        projects.forEach((project) => {
+        if (filter === '*' || filter === project.dataset.type) {
+            project.classList.remove('invisible');
+        } else {
+            project.classList.add('invisible');
+        }
+        });
+        projectContainer.classList.remove('anim-out');
+    }, 300);
 });
 
 function scrollIntoView(selector) {
